58794b3e79fc86f2d6a86c8e75e18ae0f822772b
Aaron Bedra <aaron@aaronbedra.com>
2010-09-03 11:07:00 -0400

    You don't need hiccup for the basic example

--- basic/project.clj
+++ basic/project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.2.5"]
-                 [compojure "0.4.1"]
-                 [hiccup "0.2.6"]]
+                 [compojure "0.4.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
